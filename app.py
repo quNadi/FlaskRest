63f63ff6a94af4cba5cb67df17cbf6472d77af03
ullu <ula.lukierska@gmail.com>
2020-12-22 00:27:01 +0100

    del web form

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask,request,jsonify,redirect,render_template
-
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app=Flask(__name__)
-
+bootstrap=Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///noteerrr.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
@@ -20,15 +20,15 @@ def add_data():
     new_data=DataModel(name,content)
     db.session.add(new_data)
     db.session.commit()
-    return data_schema.jsonify(new_data)
+    return redirect('/')
 
 @app.route('/',methods=['GET'])
 def get_data():
         all_data=DataModel.query.all()
         result=data_all_schema.dump(all_data)
-        return render_template('index.html',datasets=all_data)
+        return render_template('base.html',datasets=all_data)
 
-@app.route('/datapi/<id>',methods=['PUT'])
+@app.route('/datapi/<id>',methods=['PUT'])       #API
 def update_data(id):
     data=DataModel.query.get(id)
     name=request.json['name']
@@ -38,12 +38,12 @@ def update_data(id):
     db.session.commit()
     return data_schema.jsonify(data)
 
-@app.route('/datapi/<id>',methods=['DELETE'])
+@app.route('/delete/<int:id>')
 def delete_data(id):
     data=DataModel.query.get(id)
     db.session.delete(data)
     db.session.commit()
-    return data_schema.jsonify(data)
+    return redirect('/')
 
 
 if __name__=="__main__":
